98f30bcd788bd86869d80d2709b18f9fda1b2dee
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-05-13 17:47:12 +0300

    Make button able to togle the value of display of content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+
+const button = document.querySelector('button');
+const content = document.querySelector('.content');
+
+button.addEventListener('click', () => {
+  content.style.display = (content.style.display === 'none') ? 'block' : 'none';
+});
